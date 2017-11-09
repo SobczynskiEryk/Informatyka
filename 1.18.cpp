128ae5347b0e7f877a0dfc1e8b7618b1ff8fbab9
SobczynskiEryk <32760328+SobczynskiEryk@users.noreply.github.com>
2017-11-09 22:10:37 +0100

    Add files via upload

---
+++ 1.18.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b,c;
+    cout<<"Podaj 3 dlugosci bokow trojkata"<<endl;
+    cin>>a>>b>>c;
+    if ((a+b)>c && (a+c)>b && (b+c)>a) {
+        cout<<"Mozna utworzyc trojkat"<<endl;
+    } else {
+    cout<<"Nie mozna utworzyc trojkata"<<endl;
+    }
+    return 0;
+}
