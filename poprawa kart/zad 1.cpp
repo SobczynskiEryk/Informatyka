653c678361d96ab0eb3abfca4cf6973eeb9e3b9e
SobczynskiEryk <32760328+SobczynskiEryk@users.noreply.github.com>
2017-12-01 08:52:55 +0100

    Add files via upload

---
+++ poprawa kart/zad 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  for(int i=-3; i<=24; i=i+3){
+    if (i!=3 && i!=18)
+    if(i==24){
+        cout<<i;
+    }else
+    cout<<i<<",";
+  }
+
+    return 0;
+}
