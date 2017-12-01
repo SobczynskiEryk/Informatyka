5b2e9180c43f20d5119996d50115a80b1e336d0a
SobczynskiEryk <32760328+SobczynskiEryk@users.noreply.github.com>
2017-12-01 09:03:16 +0100

    Add files via upload

---
+++ poprawa kart/zad 1 Wresja 2.0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  for(int i=-3; i<=24; i=i+3){
+    if (i!=3 && i!=18 && i%2!=0)
+    if(i==21){
+        cout<<i;
+    }else
+    cout<<i<<",";
+
+  }
+
+    return 0;
+}
