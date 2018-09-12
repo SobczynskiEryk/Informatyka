e1d542f1d431895305e921a03d3e18a7907c6b89
SobczynskiEryk <32760328+SobczynskiEryk@users.noreply.github.com>
2018-09-12 12:02:55 +0200

    Add files via upload

---
+++ klasa 2/lekcja 1/potrgi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include<cmath>
+
+using namespace std;
+int potega(int a,int n)
+{
+ int p=1;
+ for(int i=1;i<=n;i++)
+ p=p*a;
+ return p;
+}
+int main(){
+
+
+    int a;
+    cin>>a;
+    cout<<potega(a,3)<<endl;
+    return 0;
+}
